ee3e44a9a47b71fa3f0fbed054c8d3f5bd31d34e
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-04-28 00:42:12 +0600

    post and comments added

--- src/components/dashboard/PostAndComments/FilterButton.jsx
+++ src/components/dashboard/PostAndComments/FilterButton.jsx
@@ -7,7 +7,6 @@ export default function FilterButton({ title, to, icon }) {
       <span
         className="group block cursor-pointer text-red-400 hover:text-primary"
         style={{ marginRight: "148", marginTop: "5px" }}
-        onclick="changeFav(this, 1);"
       >
         <span className="hidden group-hover:block absolute right-0 top-8 rounded shadow-xl bg-light-tertiary dark:bg-dark-tertiary text-xs text-gray-700 dark:text-dark-paragraph p-3">
           {title}
